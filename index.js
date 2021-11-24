a07522e38d4611f9d0602d94993e2a70fdad7cfe
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2021-11-24 11:36:13 -0500

    commit tree 228689df709c546df089e2efeb077bc417e29b85

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+// CODE
+
+//declare global variabls
+let pickup;
+const hq = 42;
+let fare;
+
+function distanceFromHqInBlocks(value = 50){
+    //returns the number of blocks given a value
+    if (value >= hq) {
+        return value - hq;
+    }
+    else{
+        return hq - value;
+    }
+}
+
+function distanceFromHqInFeet(value) {
+    //returns the calculated value of feet using distanceFromHqInBlocks
+    return 264 * distanceFromHqInBlocks(value);
+}
+
+function distanceTravelledInFeet(start, end) {
+    //returns the nu9mber of feet traveled 
+    return Math.abs((264 * start) - (264 * end));
+    
+}
+
+function calculatesFarePrice (start, end) {
+    //returns the fare for the customer
+    let  distance = distanceTravelledInFeet(start, end);
+
+    if (distance < 400) {
+        return 0;
+    }
+    else if (distance >= 400 && distance <= 2000){
+        return .02 * (distance - 400);
+    }
+    else if(distance > 2000 && distance <= 2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
+
